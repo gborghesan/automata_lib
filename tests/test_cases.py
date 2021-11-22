90b8e1e4465aeb5068d43b69a947db0c3957ad60
gborghesan <gianni.borghesan@kuleuven.be>
2021-11-23 00:16:35 +0100

    test for nested fms

--- tests/test_cases.py
+++ tests/test_cases.py
@@ -298,9 +298,196 @@ class FSMTests(unittest.TestCase):
         fsm.queque_event('e_sub_1')
         fsm.step()
         self.assertEqual(fsm.current_state,'s1')
-        self.assertIsNone(subfsm.current_state)
+        self.assertIsNone(subfsm.current_state)       
+        automata_lib.state.automatic_execution= True  
+    
+    def test_check_transitions_subfsm_nested(self):
+        [l1,s1]=setupState()
+        [l2,s2]=setupState()
+        [l3,s3]=setupState()
+        [l4,s4]=setupState()
+        [l5,s5]=setupState()
+        lsub3={'f_entry':False,'f_exit':False}
+        lsub2={'f_entry':False,'f_exit':False}
+        subfsm3=FSM(states={'s3':s3, 's4':s4},
+                   entry=partial(set_to_true,flag='f_entry',l=lsub3),
+                   exit=partial(set_to_true,flag='f_exit',l=lsub3),
+                   transitions={'e_3_4':Transition('s3','s4')},
+                   init_state='s3')
+        subfsm2=FSM(states={'s2':s2,'sub3':subfsm3},
+                   entry=partial(set_to_true,flag='f_entry',l=lsub2),
+                   exit=partial(set_to_true,flag='f_exit',l=lsub2),
+                   transitions={'e_2_sub':Transition('s2','sub3')},
+                   init_state='s2' )
+        fsm=FSM(states={'s1':s1,'sub2':subfsm2,'s5':s5},
+                transitions={'e_1_sub':Transition('s1','sub2'),
+                             'e_sub_5':Transition('sub2','s5')},
+                init_state='s1')
         
+        fsm.step()
+        self.assertEqual(fsm.current_state,'s1')
+        self.assertIsNone(subfsm2.current_state)
+        self.assertIsNone(subfsm3.current_state)
+        self.assertEqual(l1,{'f_entry': True, 'f_doo': True, 'f_exit': False})
+        self.assertEqual(l2,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l3,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l4,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': False, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': False, 'f_exit': False})
+        
+        fsm.queque_event('e_1_sub')
+        
+        fsm.step()
+        self.assertEqual(fsm.current_state,'sub2')
+        self.assertEqual(subfsm2.current_state,'s2')
+        self.assertIsNone(subfsm3.current_state)
+        self.assertEqual(l1,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l2,{'f_entry': True, 'f_doo': True, 'f_exit': False})
+        self.assertEqual(l3,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l4,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': True, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': False, 'f_exit': False})
+        
+        fsm.queque_event('e_2_sub')
+        
+        fsm.step()
+        self.assertEqual(fsm.current_state,'sub2')
+        self.assertEqual(subfsm2.current_state,'sub3')
+        self.assertEqual(subfsm3.current_state,'s3')
+        self.assertEqual(l1,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l2,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l3,{'f_entry': True, 'f_doo': True, 'f_exit': False})
+        self.assertEqual(l4,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': True, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': True, 'f_exit': False})
+        
+        fsm.queque_event('e_3_4')
+        
+        fsm.step()
+        self.assertEqual(fsm.current_state,'sub2')
+        self.assertEqual(subfsm2.current_state,'sub3')
+        self.assertEqual(subfsm3.current_state,'s4')
+        self.assertEqual(l1,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l2,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l3,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l4,{'f_entry': True, 'f_doo': True, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': True, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': True, 'f_exit': False})
+        
+        fsm.queque_event('e_sub_5')
+        
+        fsm.step()
+        self.assertEqual(fsm.current_state,'s5')
+        self.assertIsNone(subfsm2.current_state)
+        self.assertIsNone(subfsm3.current_state)
+        self.assertEqual(l1,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l2,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l3,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l4,{'f_entry': True, 'f_doo': True, 'f_exit': True})
+        self.assertEqual(l5,{'f_entry': True, 'f_doo': True, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': True, 'f_exit': True})
+        self.assertEqual(lsub3,{'f_entry': True, 'f_exit': True})
+    
+        
+    
+    def test_check_transitions_subfsm_nested_no_function_calls(self):
+            
+        [l1,s1]=setupState()
+        [l2,s2]=setupState()
+        [l3,s3]=setupState()
+        [l4,s4]=setupState()
+        [l5,s5]=setupState()
+        lsub3={'f_entry':False,'f_exit':False}
+        lsub2={'f_entry':False,'f_exit':False}
+        subfsm3=FSM(states={'s3':s3, 's4':s4},
+                   entry=partial(set_to_true,flag='f_entry',l=lsub3),
+                   exit=partial(set_to_true,flag='f_exit',l=lsub3),
+                   transitions={'e_3_4':Transition('s3','s4')},
+                   init_state='s3')
+        subfsm2=FSM(states={'s2':s2,'sub3':subfsm3},
+                   entry=partial(set_to_true,flag='f_entry',l=lsub2),
+                   exit=partial(set_to_true,flag='f_exit',l=lsub2),
+                   transitions={'e_2_sub':Transition('s2','sub3')},
+                   init_state='s2' )
+        fsm=FSM(states={'s1':s1,'sub2':subfsm2,'s5':s5},
+                transitions={'e_1_sub':Transition('s1','sub2'),
+                             'e_sub_5':Transition('sub2','s5')},
+                init_state='s1')
+        
+        automata_lib.state.automatic_execution= False   
+        fsm.step()
+        self.assertEqual(fsm.current_state,'s1')
+        self.assertIsNone(subfsm2.current_state)
+        self.assertIsNone(subfsm3.current_state)
+        self.assertEqual(l1,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l2,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l3,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l4,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': False, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': False, 'f_exit': False})
+        
+        fsm.queque_event('e_1_sub')
+        
+        fsm.step()
+        self.assertEqual(fsm.current_state,'sub2')
+        self.assertEqual(subfsm2.current_state,'s2')
+        self.assertIsNone(subfsm3.current_state)
+        self.assertEqual(l1,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l2,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l3,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l4,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': False, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': False, 'f_exit': False})
+        
+        fsm.queque_event('e_2_sub')
+        
+        fsm.step()
+        self.assertEqual(fsm.current_state,'sub2')
+        self.assertEqual(subfsm2.current_state,'sub3')
+        self.assertEqual(subfsm3.current_state,'s3')
+        self.assertEqual(l1,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l2,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l3,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l4,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': False, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': False, 'f_exit': False})
+        
+        fsm.queque_event('e_3_4')
+        
+        fsm.step()
+        self.assertEqual(fsm.current_state,'sub2')
+        self.assertEqual(subfsm2.current_state,'sub3')
+        self.assertEqual(subfsm3.current_state,'s4')
+        self.assertEqual(l1,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l2,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l3,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l4,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': False, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': False, 'f_exit': False})
+        
+        fsm.queque_event('e_sub_5')
+        
+        fsm.step()
+        self.assertEqual(fsm.current_state,'s5')
+        self.assertIsNone(subfsm2.current_state)
+        self.assertIsNone(subfsm3.current_state)
+        self.assertEqual(l1,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l2,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l3,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l4,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(l5,{'f_entry': False, 'f_doo': False, 'f_exit': False})
+        self.assertEqual(lsub2,{'f_entry': False, 'f_exit': False})
+        self.assertEqual(lsub3,{'f_entry': False, 'f_exit': False})
         automata_lib.state.automatic_execution= True   
+
 if __name__ == '__main__':
     import rostest
     rostest.rosrun('automata_lib', 'test_StateConstructor', StateConstructor)
